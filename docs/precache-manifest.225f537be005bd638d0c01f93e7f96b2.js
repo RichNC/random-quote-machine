9d40ce61a2c83d0b7beeb820af9790f8f205c104
Rich Cherry <rich.m.cherry@gmail.com>
2020-08-30 00:34:52 -0600

    Build for production

---
+++ docs/precache-manifest.225f537be005bd638d0c01f93e7f96b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70feef85453351a3a17e655a6cd7b782",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "b05c4cacaea30e1303f5",
+    "url": "/random-quote-machine/static/css/main.711477d4.chunk.css"
+  },
+  {
+    "revision": "1de4b1557851be60ec04",
+    "url": "/random-quote-machine/static/js/2.2f20868d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.2f20868d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b05c4cacaea30e1303f5",
+    "url": "/random-quote-machine/static/js/main.e1119db5.chunk.js"
+  },
+  {
+    "revision": "81a81eeacc763523a4c2",
+    "url": "/random-quote-machine/static/js/runtime-main.d8166d98.js"
+  }
+]);
